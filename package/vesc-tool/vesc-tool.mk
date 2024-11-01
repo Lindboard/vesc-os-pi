a1278a35f6dc91dc79a8cddbc4d52e331d0be1cc
laxsjo <rasmus.soderhielm@gmail.com>
2024-11-01 11:25:02 +0100

    Bump vesc_tool

--- package/vesc-tool/vesc-tool.mk
+++ package/vesc-tool/vesc-tool.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-VESC_TOOL_VERSION = 42a3a9489067635b31a8c73b796159bdeec79c86
+VESC_TOOL_VERSION = 9ee74f5451536e07e653bc5179a6ed6be805bc1a
 VESC_TOOL_SITE = $(call github,vedderb,vesc_tool,$(VESC_TOOL_VERSION))
 VESC_TOOL_LICENSE = GPLv3
 VESC_TOOL_LICENSE_FILES = LICENSE
